a2e528f23438224be45c2a70e30c346409a026ef
mohamed <32775877+mohamedkhattab2@users.noreply.github.com>
2022-06-03 21:28:34 +0200

    addBasePlusCommissionEmployee

--- src/com/inheritance/Main.java
+++ src/com/inheritance/Main.java
@@ -3,8 +3,9 @@ package com.inheritance;
 public class Main {
 
     public static void main(String[] args) {
-        CommissionEmployee employee = new CommissionEmployee
-                ("ahmed", "Abdo", "222-22-2222", 10000, 0.06);
+        BasePlusCommissionEmployee employee = new BasePlusCommissionEmployee ("ahmed", "Abdo", "222-22-2222",
+                10000, 0.06, 300);
+
 
         System.out.println("Employee information obtained by get methods:");
         System.out.printf("%n%s %s%n", "First name is",
@@ -19,10 +20,14 @@ public class Main {
         System.out.printf("%s %.2f%n", "Commission rate is",
                 employee.getCommissionRate());
 
+        System.out.printf("%s %.2f%n", "Base salary is",
+                employee.getBaseSalary());
+        employee.setBaseSalary(10000);
+
         employee.setCommissionRate(.1);
         employee.setGrossSales(5000);
         System.out.printf("%n%s:%n%n%s%n",
                 "Update employee information obtained by toString", employee);
     }// end main
 
-}// end class CommissionEmployeeTest
+}// end class BasTest
